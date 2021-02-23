0ec9bb761f4c76f7828bad989c1d3d82d3e802b9
vaibhavtailor <77836750+vaibhavtailor@users.noreply.github.com>
2021-02-23 18:10:34 -0500

    Lab Week 5 Complete

---
+++ src/Student.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int studentNumber;
+    private String interests;
+
+    /**
+     * Creating a constructor
+     */
+    public Student(String firstName, String lastName, int studentNumber, String interests) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setStudentNumber(studentNumber);
+        setInterests(interests);
+    }
+
+    /**
+     * Creating setter and getters for each field.
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getInterests() {
+        return interests;
+    }
+
+    public void setInterests(String interests) {
+        interests = interests.toLowerCase();
+        List<String> validInterests = getInterest();
+        if (validInterests.contains(interests))
+            this.interests = interests;
+        else
+            throw new IllegalArgumentException(interests + "is not valid, valid Interests are: " + validInterests);
+    }
+
+    public static List<String> getInterest() {
+        return Arrays.asList("coding", "gaming", "hiking", "skiing", "reading", "playing", "writing", "singing");
+    }
+
+    public String toString()
+    {
+        return "Your name is : " + firstName + " " + lastName + "\n" + "Your Student Number is : " + studentNumber + "\n" + "Your interest is : " + interests;
+    }
+
+}
+
---
+++ src/StudentTester.java
@@ -0,0 +1,7 @@
+public class StudentTester {
+    public static void main(String[] args) {
+        Student info = new Student("Vaibhav", "Tailor", 200458209, "Coding");
+
+        System.out.println(info);
+    }
+}
